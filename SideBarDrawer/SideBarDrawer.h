69b2ba9f76735ca9b396095d0a3767b3d2979eed
Manu Singh <manukumar@bikayi.com>
2022-06-26 16:39:05 +0530

    Initial Commit

---
+++ SideBarDrawer/SideBarDrawer.h
@@ -0,0 +1,18 @@
+//
+//  SideBarDrawer.h
+//  SideBarDrawer
+//
+//  Created by Manu Singh on 26/06/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SideBarDrawer.
+FOUNDATION_EXPORT double SideBarDrawerVersionNumber;
+
+//! Project version string for SideBarDrawer.
+FOUNDATION_EXPORT const unsigned char SideBarDrawerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SideBarDrawer/PublicHeader.h>
+
+
